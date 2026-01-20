5f2bd296e43474e2d1a7465a60e09630ee1e743c
bryce <bryce.hoehn@mailbox.org>
2026-01-20 12:29:13 -0500

    test

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
+from enum import Enum
 import os
 import asyncio
-from discord.ext import commands
 from dotenv import load_dotenv
 
 from transcription import transcribe_with_timestamps, combine_transcripts
@@ -15,49 +15,57 @@ except RuntimeError:
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
 
-intents = discord.Intents.default()
-intents.voice_states = True
-intents.guilds = True
-intents.message_content = True
-
-bot = discord.Bot(intents=intents)
+bot = discord.Bot(intents=discord.Intents.all())
 
 # Global state
 connections = {}
 
-async def once_done(sink: discord.sinks, channel: discord.TextChannel, *args):
-    await sink.vc.disconnect()  # Disconnect from the voice channel.
+async def finished_callback(sink, channel: discord.TextChannel, *args):
+    recorded_users = [f"<@{user_id}>" for user_id, audio in sink.audio_data.items()]
+    await sink.vc.disconnect()
+    files = [
+        discord.File(audio.file, f"{user_id}.{sink.encoding}")
+        for user_id, audio in sink.audio_data.items()
+    ]
+    await channel.send(
+        f"Finished! Recorded audio for {', '.join(recorded_users)}.", files=files
+    )
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user.name} ({bot.user.id})")
 
-@bot.command()
-async def record(ctx):  # If you're using commands.Bot, this will also work.
+@bot.slash_command(name="record")
+async def record(ctx: discord.ApplicationContext):
+    """Record your voice!"""
     voice = ctx.author.voice
 
     if not voice:
-        await ctx.respond("You aren't in a voice channel!")
+        return await ctx.respond("You're not in a vc right now")
 
-    vc = await voice.channel.connect()  # Connect to the voice channel the author is in.
-    connections.update({ctx.guild.id: vc})  # Updating the cache with the guild and channel.
+    vc = await voice.channel.connect()
+    connections.update({ctx.guild.id: vc})
 
+    vc.play("obama.mp3")
+            
     vc.start_recording(
-        discord.sinks.OGGSink(),  # The sink type to use.
-        once_done,  # What to do once done.
-        ctx.channel  # The channel to disconnect from.
+        discord.sinks.OGGSink,
+        finished_callback,
+        ctx.channel,
     )
-    await ctx.respond("Started recording!")
 
-@bot.command()
-async def stop_recording(ctx):
-    if ctx.guild.id in connections:  # Check if the guild is in the cache.
+    await ctx.respond("The recording has started!")
+
+@bot.slash_command(name="stop")
+async def stop(ctx: discord.ApplicationContext):
+    """Stop recording."""
+    if ctx.guild.id in connections:
         vc = connections[ctx.guild.id]
-        vc.stop_recording()  # Stop recording, and call the callback (once_done).
-        del connections[ctx.guild.id]  # Remove the guild from the cache.
-        await ctx.delete()  # And delete.
+        vc.stop_recording()
+        del connections[ctx.guild.id]
+        await ctx.delete()
     else:
-        await ctx.respond("I am currently not recording here.")  # Respond with this if we aren't recording.
+        await ctx.respond("Not recording in this guild.")
 
 if __name__ == "__main__":
     token = os.getenv("DISCORD_TOKEN")
